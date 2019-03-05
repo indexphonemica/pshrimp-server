9b1160f16b808d39a2fe2c0bb0b725d50c30894c
defseg <6988161+defseg@users.noreply.github.com>
2019-03-05 00:49:02 -0500

    Aggregate raw SQL results - return one row per language, with a phonemes array, instead of one row per language-phoneme pair

--- app.js
+++ app.js
@@ -60,12 +60,36 @@ app.get('/', function (req, res) {
 	res.send('hello world');
 })
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/query/:query', async function (req, res) {
 	const query_text = decode(req.params.query);
 	const query = psentence.parse(query_text);
 	const query_sql = psherlock.build_sql(query);
 	const results = await db.allAsync(query_sql);
-	res.send(results);
+
+	// SQL will return one row per phoneme.
+	// Aggregate these so there's a phonemes value with an array.
+	// This relies on language rows always being contiguous!
+	var new_results = [];
+	var processed = new Set();
+	for (let i of results) {
+		if (!processed.has(i.id)) {
+			if (lang) new_results.push(lang);
+
+			var {phoneme, ...lang} = i; // really weird destructuring syntax - `lang` ends up with all the row props except `phoneme` 
+			processed.add(i.id);
+			if (i.phoneme) lang.phonemes = [];
+		}
+		if (i.phoneme) lang.phonemes.push(i.phoneme);
+	}
+	new_results.push(lang);
+
+	res.send(new_results);
 })
 
 app.get('/language/:language', async function (req, res) {
@@ -82,6 +106,5 @@ app.get('/language/:language', async function (req, res) {
 function decode(thing) {
 	return decodeURIComponent(thing.replace(/\\e/g,'=').replace(/\\\+/g,'&').replace(/\\\\/g,'\\'));
 }
-
 
 app.listen(port, () => console.log(`The great Pshrimp awaketh on port ${port}!`));
