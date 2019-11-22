dea63b21a7ef5f56e60271b2aff67d813c129275
defseg <6988161+defseg@users.noreply.github.com>
2019-11-22 06:39:49 -0500

    Update property columns

--- props.js
+++ props.js
@@ -1,34 +1,60 @@
 // This could stand to be automated.
 // See also: db_info.js
 
+// *** If you add any prop_columns here, document it in the client. ***
+
+// Not every field needs to be searchable. Most of the IPHON source stuff (e.g. volume, number) isn't.
+// This is basically arbitrary.
+
 // Structure: 'field_name': ['table', 'column'] if the column name isn't the field name
 //            'field_name': 'column' if the column name isn't the field name
-const PROP_COLUMNS = {
-	'id':            'doculects'
-,	'source':        'doculects'
-,	'glottocode':    'doculects'
-,	'iso6393':       'doculects'
-,   'language_name': 'doculects'
-,   'family':        'languages'
-,   'genus':         'languages'
-,   'macroarea':     'languages'
-,   'country_id':    ['countries', 'id']
-,   'country':       ['countries', 'name']
+
+if (!!(+process.env.IS_IPHON)) {
+    var PROP_COLUMNS = {
+        'inventory_id':     'doculects'
+    ,   'language_name':    'doculects'
+    ,   'glottocode':       'doculects'
+    ,   'dialect':          'doculects'
+    ,   'dialect_name':     'doculects'
+    ,   'source_bibkey':    'doculects'
+    ,   'source_url':       'doculects'
+    ,   'source_doi':       'doculects'
+    ,   'name':             'languages'
+    ,   'iso6393':          'languages'
+    ,   'family':           'languages'
+    ,   'genus':            'languages'
+    ,   'macroarea':        'languages'
+    ,   'country_id':       ['countries', 'id']
+    ,   'country':          ['countries', 'name']
+    }
+} else {
+    var PROP_COLUMNS = {
+        'id':            'doculects'
+    ,   'source':        'doculects'
+    ,   'glottocode':    'doculects'
+    ,   'iso6393':       'doculects'
+    ,   'language_name': 'doculects'
+    ,   'family':        'languages'
+    ,   'genus':         'languages'
+    ,   'macroarea':     'languages'
+    ,   'country_id':    ['countries', 'id']
+    ,   'country':       ['countries', 'name']
+    }   
 }
 
 /** Finds the table that contains information on a given property name.
     Pshrimp abstracts away from tables, so each propqueryable column should have a different name.
 */
 module.exports.table = function (prop_name) {
-	if (typeof PROP_COLUMNS[prop_name] === 'string') return PROP_COLUMNS[prop_name];
-	if (typeof PROP_COLUMNS[prop_name] === 'object') return PROP_COLUMNS[prop_name][0];
-	throw new Error('Undefined property table'); // TODO define an error for this?
+    if (typeof PROP_COLUMNS[prop_name] === 'string') return PROP_COLUMNS[prop_name];
+    if (typeof PROP_COLUMNS[prop_name] === 'object') return PROP_COLUMNS[prop_name][0];
+    throw new Error('Undefined property table'); // TODO define an error for this?
 }
 
 /** Finds the column name that contains information on a given property name.
 */
 module.exports.column = function (prop_name) {
-	if (typeof PROP_COLUMNS[prop_name] === 'string') return prop_name;
-	if (typeof PROP_COLUMNS[prop_name] === 'object') return PROP_COLUMNS[prop_name][1];
-	throw new Error('Undefined property column');
+    if (typeof PROP_COLUMNS[prop_name] === 'string') return prop_name;
+    if (typeof PROP_COLUMNS[prop_name] === 'object') return PROP_COLUMNS[prop_name][1];
+    throw new Error('Undefined property column');
 }
